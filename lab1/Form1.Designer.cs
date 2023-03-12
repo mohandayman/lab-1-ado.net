fd63201ecc9471047da68ff3bab96e24a73f7bd9
mohandayman <66260018+mohandayman@users.noreply.github.com>
2023-03-12 07:55:26 +0200

    k

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.sqlInsertCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlUpdateCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDeleteCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDataAdapter1 = new System.Data.SqlClient.SqlDataAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet11 = new lab1.DataSet1();
+            this.connect = new System.Windows.Forms.Button();
+            this.dept_ = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dept_name = new System.Windows.Forms.TextBox();
+            this.dept_num = new System.Windows.Forms.TextBox();
+            this.emp_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emp_id = new System.Windows.Forms.TextBox();
+            this.empId = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = "select * from employee";
+            this.sqlSelectCommand1.Connection = this.sqlConnection1;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=.;Initial Catalog=mohand;Integrated Security=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlInsertCommand1
+            // 
+            this.sqlInsertCommand1.CommandText = "INSERT INTO [employee] ([id], [empName], [deptId]) VALUES (@id, @empName, @deptId" +
+    ");\r\nSELECT id, empName, deptId FROM employee WHERE (id = @id)";
+            this.sqlInsertCommand1.Connection = this.sqlConnection1;
+            this.sqlInsertCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@id", System.Data.SqlDbType.Int, 0, "id"),
+            new System.Data.SqlClient.SqlParameter("@empName", System.Data.SqlDbType.VarChar, 0, "empName"),
+            new System.Data.SqlClient.SqlParameter("@deptId", System.Data.SqlDbType.Int, 0, "deptId")});
+            // 
+            // sqlUpdateCommand1
+            // 
+            this.sqlUpdateCommand1.CommandText = resources.GetString("sqlUpdateCommand1.CommandText");
+            this.sqlUpdateCommand1.Connection = this.sqlConnection1;
+            this.sqlUpdateCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@id", System.Data.SqlDbType.Int, 0, "id"),
+            new System.Data.SqlClient.SqlParameter("@empName", System.Data.SqlDbType.VarChar, 0, "empName"),
+            new System.Data.SqlClient.SqlParameter("@deptId", System.Data.SqlDbType.Int, 0, "deptId"),
+            new System.Data.SqlClient.SqlParameter("@Original_id", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "id", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_empName", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "empName", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_empName", System.Data.SqlDbType.VarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "empName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_deptId", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "deptId", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_deptId", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "deptId", System.Data.DataRowVersion.Original, null)});
+            // 
+            // sqlDeleteCommand1
+            // 
+            this.sqlDeleteCommand1.CommandText = resources.GetString("sqlDeleteCommand1.CommandText");
+            this.sqlDeleteCommand1.Connection = this.sqlConnection1;
+            this.sqlDeleteCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@Original_id", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "id", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_empName", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "empName", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_empName", System.Data.SqlDbType.VarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "empName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_deptId", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "deptId", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_deptId", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "deptId", System.Data.DataRowVersion.Original, null)});
+            // 
+            // sqlDataAdapter1
+            // 
+            this.sqlDataAdapter1.DeleteCommand = this.sqlDeleteCommand1;
+            this.sqlDataAdapter1.InsertCommand = this.sqlInsertCommand1;
+            this.sqlDataAdapter1.SelectCommand = this.sqlSelectCommand1;
+            this.sqlDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "employee", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("id", "id"),
+                        new System.Data.Common.DataColumnMapping("empName", "empName"),
+                        new System.Data.Common.DataColumnMapping("deptId", "deptId")})});
+            this.sqlDataAdapter1.UpdateCommand = this.sqlUpdateCommand1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-8, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(702, 230);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataSet11
+            // 
+            this.dataSet11.DataSetName = "DataSet1";
+            this.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(26, 373);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(113, 43);
+            this.connect.TabIndex = 1;
+            this.connect.Text = "connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // dept_
+            // 
+            this.dept_.AutoSize = true;
+            this.dept_.Location = new System.Drawing.Point(37, 28);
+            this.dept_.Name = "dept_";
+            this.dept_.Size = new System.Drawing.Size(72, 16);
+            this.dept_.TabIndex = 2;
+            this.dept_.Text = "dept _Num";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dept_Name";
+            // 
+            // dept_name
+            // 
+            this.dept_name.Location = new System.Drawing.Point(436, 22);
+            this.dept_name.Name = "dept_name";
+            this.dept_name.Size = new System.Drawing.Size(100, 22);
+            this.dept_name.TabIndex = 4;
+            // 
+            // dept_num
+            // 
+            this.dept_num.Location = new System.Drawing.Point(121, 22);
+            this.dept_num.Name = "dept_num";
+            this.dept_num.Size = new System.Drawing.Size(100, 22);
+            this.dept_num.TabIndex = 5;
+            // 
+            // emp_name
+            // 
+            this.emp_name.Location = new System.Drawing.Point(139, 85);
+            this.emp_name.Name = "emp_name";
+            this.emp_name.Size = new System.Drawing.Size(100, 22);
+            this.emp_name.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "emp_name";
+            // 
+            // emp_id
+            // 
+            this.emp_id.Location = new System.Drawing.Point(425, 88);
+            this.emp_id.Name = "emp_id";
+            this.emp_id.Size = new System.Drawing.Size(100, 22);
+            this.emp_id.TabIndex = 8;
+            // 
+            // empId
+            // 
+            this.empId.AutoSize = true;
+            this.empId.Location = new System.Drawing.Point(344, 85);
+            this.empId.Name = "empId";
+            this.empId.Size = new System.Drawing.Size(52, 16);
+            this.empId.TabIndex = 9;
+            this.empId.Text = "emp_id";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(562, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 33);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "insert ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(662, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 44);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(662, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 44);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "select";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(507, 363);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(113, 43);
+            this.close.TabIndex = 14;
+            this.close.Text = "close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.empId);
+            this.Controls.Add(this.emp_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.emp_name);
+            this.Controls.Add(this.dept_num);
+            this.Controls.Add(this.dept_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dept_);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlCommand sqlInsertCommand1;
+        private System.Data.SqlClient.SqlCommand sqlUpdateCommand1;
+        private System.Data.SqlClient.SqlCommand sqlDeleteCommand1;
+        private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DataSet1 dataSet11;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Label dept_;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dept_name;
+        private System.Windows.Forms.TextBox dept_num;
+        private System.Windows.Forms.TextBox emp_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox emp_id;
+        private System.Windows.Forms.Label empId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button close;
+    }
+}
+
---
+++ lab1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            sqlConnection1.Open();
+            sqlDataAdapter1.Fill(dataSet11);
+            sqlConnection1.Close();
+            dataGridView1.DataSource =  dataSet11.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataRow drow = dataSet11.Tables[0].NewRow();
+            drow[0] =int.Parse(emp_id.Text);
+            drow["empName"] = emp_name.Text;
+            drow["deptId"]  = int.Parse(dept_num.Text); 
+            dataSet11.Tables[0].Rows.Add(drow);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var recordId = int.Parse(emp_id.Text);
+            DataRow updated_row = dataSet11.Tables[0].Rows.Find(recordId);
+            if (updated_row != null) {
+                updated_row["empName"] = emp_name.Text;
+                updated_row["deptId"] = int.Parse(dept_num.Text);
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            sqlConnection1.Open();
+            sqlDataAdapter1.Update(dataSet11);
+            sqlConnection1.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sqlConnection1.Open();
+
+        }
+    }
+}
